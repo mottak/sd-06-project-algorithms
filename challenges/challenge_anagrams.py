6590b1ff4724cfa4827051d839beb84997973502
Tiago Berwanger <berwangertiago@gmail.com>
2021-06-02 18:55:45 -0300

    requirement 3 - refactored

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,7 +1,11 @@
+# def is_anagram(first_string, second_string):
+#     first = sorted(first_string)
+#     second = sorted(second_string)
+#     if first == second:
+#         return True
+#     else:
+#         return False
+
+
 def is_anagram(first_string, second_string):
-    first = sorted(first_string)
-    second = sorted(second_string)
-    if first == second:
-        return True
-    else:
-        return False
+    return sorted(first_string) == sorted(second_string)
