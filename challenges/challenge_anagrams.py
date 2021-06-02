7da273f9003972e705e0c49f59ed3e56d149ca5c
thalesgr <thalesgr@gmail.com>
2021-06-02 15:33:56 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if not first_string or not second_string:
+        return False
+    if len(first_string) == len(second_string):
+        if sorted(first_string) == sorted(second_string):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+# baseado no site abaixo:
+# https://www.programiz.com/python-programming/examples/anagram
