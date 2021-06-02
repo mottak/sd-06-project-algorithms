dd614eb9bdb67d1fcf8027c3f9e7908a9471834b
mottak <kissylamottapereira@gmail.com>
2021-06-02 17:40:58 -0300

    requisito 1: consta estudantes em determinado horario

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    conta_estudante = 0
+    if start_time == [] or target_time == 0:
+        return 0
+    for start, end in zip(start_time, end_time):
+        if target_time >= start and target_time <= end:
+            conta_estudante += 1
+    return conta_estudante
+
+# https://www.w3schools.com/python/ref_func_zip.asp
+# a Função zip() retorna um zip object, que é um itrador de
+# tuplas,onde o primeiro item do iterador
+# é emparelhado ao primeiro array e o segundo item do iterador
+# é emparelhado com o segundo array
