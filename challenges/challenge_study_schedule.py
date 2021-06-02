11ea1305928a718e9e8e76508fb69f5d5d8fa0e1
thalesgr <thalesgr@gmail.com>
2021-06-02 15:01:14 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    if not target_time:
+        return 0
+    for time in range(len(start_time)):
+        if start_time[time] <= target_time <= end_time[time]:
+            count += 1
+
+    return count
