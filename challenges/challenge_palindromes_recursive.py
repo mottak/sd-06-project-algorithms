f9355a504c8bdd793ba849914c9ea9ab2252f7f7
danieleperse <danieleperse3@gmail.com>
2021-06-02 20:58:42 -0300

    requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    if len(word) == 1:
+        return True
+
+    if word[low] == word[high]:
+        substring = word[1:len(word) - 1]
+        return is_palindrome_recursive(substring, 0, len(substring) - 1)
+
+    return False
