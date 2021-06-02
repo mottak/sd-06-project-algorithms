fc6f4032a26898778b2a4d94759708b139b3f14c
danieleperse <danieleperse3@gmail.com>
2021-06-02 17:11:52 -0300

    primeiro commit

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    list_tuplas = []
+    for start_time, end_time in zip(start_time, end_time):
+        list_tuplas.append((start_time, end_time))
+
+    result = 0
+    for i in list_tuplas:
+        if i[0] <= target_time <= i[1]:
+            result += 1
+
+    return result
