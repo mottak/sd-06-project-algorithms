54e5e73f9f00160ad1b3ee2842a4ed98d9091c16
FlavioSugano <flavio.sugano@gmail.com>
2021-06-02 19:17:14 -0300

    feat: challenge 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+
+    if not target_time:
+        return 0
+
+    for start_t, stop_t in zip(start_time, end_time):
+        if start_t <= target_time <= stop_t:
+            counter += 1
+
+    return counter
