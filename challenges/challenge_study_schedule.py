fb5ace4ea53698f08d8372713e950fc22ee473b0
ricknois <rickssti@outlook.com>
2021-06-02 18:34:32 -0300

    :sparkles: Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if start_time == [] or target_time == 0:
+        return 0
+
+    for index, hour in enumerate(start_time):
+        if target_time <= end_time[index] and target_time >= hour:
+            counter += 1
+
+    return counter
