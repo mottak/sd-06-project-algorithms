db904e476541a0b17e14c5ee530b302dea7e9ee8
Cyrano webdev <cyrano@cyranowebdev.com>
2021-06-02 14:50:43 -0300

    init project + req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,15 @@
+from typing import Counter
+
+
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    """ Recebe um horário target_time e retorna quantos estudantes
+        estavam estudando naquele horário. """
+    if not target_time or  not start_time:
+        return 0
+
+    counter = 0
+    for index, hour in enumerate(start_time):
+        if target_time >= hour and target_time <= end_time[index]:
+            counter += 1
+
+    return counter
