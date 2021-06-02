fd9c827a061b4313555ecc3d8955c2ca5534a7b7
Cyrano webdev <cyrano@cyranowebdev.com>
2021-06-02 15:59:21 -0300

    feat: bonus req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,14 @@
+from challenges.challenge_anagrams import quicksort
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    """ Recebe uma lista de números inteiros >= 1 como parâmetro,
+        ordena e retorna um número se ele for repetido na lista """
+    if not nums or isinstance(nums, str) or len(nums) < 2:
+        return False
+
+    sort_nums = quicksort(nums)
+
+    if sort_nums[0] == sort_nums[1]:
+        return sort_nums[0]
+    next_step = sort_nums[1:]
+    return find_duplicate(next_step)
