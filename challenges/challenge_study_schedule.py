1e4bdeea8e0a26eeefbb1b8c8c948556a289d78f
Tiago Berwanger <berwangertiago@gmail.com>
2021-06-02 16:47:10 -0300

    requirement 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    best_time = 0
+    if start_time == [] or target_time == 0:
+        return 0
+    for x1, x2 in zip(start_time, end_time):
+        if target_time >= x1 and target_time <= x2:
+            best_time += 1
+    return best_time
+
+
+start_time = [2, 1, 2, 1, 4, 4]
+end_time = [2, 2, 3, 5, 5, 5]
+print(study_schedule(start_time, end_time, 5))
