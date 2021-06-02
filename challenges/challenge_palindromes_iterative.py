c1eb5d679435d65da1827142e65e6085132a8d7a
Cyrano webdev <cyrano@cyranowebdev.com>
2021-06-02 16:06:18 -0300

    feat: bonus req 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,14 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    """ Dada uma string, retorna se ela é
+        um palíndromo usando lógica iterativa. """
+
+    if not word:
+        return False
+    char_index_to_compare = -1
+    for index in range(len(word) // 2):
+        if word[index] != word[char_index_to_compare]:
+            return False
+        else:
+            char_index_to_compare -= 1
+
+    return True
