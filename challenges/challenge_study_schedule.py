da4e88832279a6d477574558f5a667a3037ed7c7
vapatty6848 <vapatty@gmail.com>
2021-06-03 13:08:10 -0300

    requisito challenge study schudele

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(start_time, end_time, target_time):
     """ Faça o código aqui. """
+    list = []
+    for start_time, end_time  in zip(start_time, end_time):
+        list.append((start_time, end_time))
+
+    cont_result = 0
+    for item in list:
+        if item[0] <= target_time <= item[1]:
+            cont_result += 1
+    return cont_result
