255e5ada03596cddc9b2623ec67d7d8dafe9ff42
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-06-02 21:52:28 -0300

    req1 e req2 parcial

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low, high):
     """ Faça o código aqui. """
+    print('low:', low)
+    print('high:', high)
+
+    if word == '' or word[low] != word[high]: return False
+    if len(word) == 1: return True 
+
+    if low < high:
+        low += 1
+        high -= 1
+        return is_palindrome_recursive(word, low, high)
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(start_time, end_time, target_time):
     """Busca horário menos utilizado pelos estudantes"""
+
+    same_time_students = 0
+
+    for student_start_time, student_end_time in zip(start_time, end_time):
+        if target_time >= student_start_time and target_time <= student_end_time:
+            same_time_students += 1
+    return same_time_students
