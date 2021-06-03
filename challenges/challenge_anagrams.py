93558f3dff2450501004841ba95aa99967186e0e
bynho25 <pablo.trybe@gmail.com>
2021-06-03 12:38:20 -0300

    Try req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,8 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    for i in second_string:
+        if i not in first_string:
+            return False
+    return True
