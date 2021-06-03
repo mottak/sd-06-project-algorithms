8a2f4ac73ec858c907a54249a906553f2afac229
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2021-06-03 17:49:41 -0300

    feat: Study Schedule (1st requirement) local approve

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,18 @@
+#!/usr/bin/env python3
+
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    if (not start_time or not end_time):
+        return 0
+
+    count = 0
+    for start, end in zip(start_time, end_time):
+        if (start <= target_time and end >= target_time):
+            count += 1
+
+    return count
+
+
+start_time = [4, 1, 3, 2]
+end_time = [4, 3, 4, 5]
+
+# print(study_schedule(start_time, end_time, 3))
