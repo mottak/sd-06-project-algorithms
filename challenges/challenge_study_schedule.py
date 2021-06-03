56046a778813c23200ebdc1a6d57285ab8557b70
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-06-03 16:51:41 -0300

    :sparkles: feat(Challenges): Implementation of study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,15 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    if start_time == 0 or target_time == 0:
+        return 0
+
+    count = 0
+
+    for x in range(0, len(start_time)):
+        index = start_time[x]
+
+        while index <= end_time[x]:
+            if index == target_time:
+                count += 1
+            index += 1
+
+    return count
