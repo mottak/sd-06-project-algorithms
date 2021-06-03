be22cff6db1480320d7fffb16908f6db59d290c2
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-06-02 22:02:24 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,19 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    has_not_start_time = len(start_time) == 0 or False
+    has_not_target_time = target_time is None or False
+
+    if has_not_start_time or has_not_target_time:
+        return 0
+
+    target_time_count = 0
+    for i in range(len(start_time)):
+        if start_time[i] <= target_time <= end_time[i]:
+            target_time_count += 1
+
+    return target_time_count
+
+
+start_time = [2, 1, 2, 1, 4, 4]
+end_time = [2, 2, 3, 5, 5, 5]
+target_time = 2
+print(study_schedule(start_time, end_time, target_time))
