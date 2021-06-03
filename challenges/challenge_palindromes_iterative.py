6f0cb440fca1ee1efc4ad83b441ddcceeb4c6440
danieleperse <danieleperse3@gmail.com>
2021-06-02 21:54:26 -0300

    req. bonus - palindromo

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,16 @@
+from math import floor
+
+
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    length_word = len(word)
+    to = floor(length_word/2)
+
+    for i in range(to):
+        substring = word[i:length_word - i]
+        if substring[0] != substring[-1]:
+            return False
+
+    return True
