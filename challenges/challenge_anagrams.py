565ce59f698cc03490e7fb77e58261153ce72233
danieleperse <danieleperse3@gmail.com>
2021-06-02 21:23:17 -0300

    requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,12 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    array1 = sorted(first_string)
+    array2 = sorted(second_string)
+
+    if len(array1) == 0 or len(array2) == 0 or len(array1) != len(array2):
+        return False
+
+    for i in range(len(array1)):
+        if array1[i] != array2[i]:
+            return False
+
+    return True
