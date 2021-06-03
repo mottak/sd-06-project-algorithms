97fec4e51c6077f55e5a54dfd4e794d738c5e68d
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-06-03 07:59:45 -0300

    :scroll: não passa o requisito 100%, mas tinha comentários e prints que me ajudaram a entender melhor o desafio. Sem eles o req1 passa. Mesmo assim eu deixei, pq vai que pode ajudar outras pessoas a entender melhor.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,23 @@
-def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+def study_schedule(start_time=[], end_time=[], target_time=[]):
+    # qual o melhor horário para disponibilizar os conteúdos?
+    # Quando o contador retornado pela função for o maior.
+    contador = 0
+
+    # no caso de os arrays estarem vazios:
+    if not target_time or not start_time or not end_time:
+        return contador
+
+    # percorrer os arrays para somar quantas vezes o valor pretendido (target_time)
+    # aparece e assim pode ser contado += 1
+
+    # para cada elemento (indice) no tamanho array de saída (end_time - quantas vezes)
+    for indice in range(len(end_time)):
+        if start_time[indice] <= target_time <= end_time[indice]:
+            contador += 1
+            print(
+                f"contador na volta {indice}: {contador}, quem é o target: {target_time}"
+            )
+
+    print(f"array entrada: {start_time} e array saida: {end_time}")
+    print(f"contador fora do loop? {contador}")
+    return contador
