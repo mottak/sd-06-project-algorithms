aad75f9b15e1e3fb3f774deb75ea067aede931c6
Marcelo Pessini <marceloppessini@gmail.com>
2021-06-04 16:13:08 -0300

    Algoritmos Projeto

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    if len(word) == 1:
+        return True
+    if word[low] != word[high]:
+        return False
+    if low == high:
+        return True
+    if low < high + 1:
+        return is_palindrome_recursive(word, low + 1, high - 1)
+    
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,7 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    # Função zip ensinada pelo Bergamini
+    counter = 0
+    for start, end in zip(start_time, end_time):
+        if target_time >= start and target_time <= end:
+            counter += 1
+    return counter
