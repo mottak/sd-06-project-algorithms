aafa26340394fe844d113e5e9c36b1a1f8a9da2d
Marcelo Pessini <marceloppessini@gmail.com>
2021-06-04 16:17:53 -0300

    arrumando snowflakes

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,12 +1,9 @@
 def is_palindrome_recursive(word, low, high):
     if not word:
         return False
-    if len(word) == 1:
-        return True
     if word[low] != word[high]:
         return False
     if low == high:
         return True
     if low < high + 1:
         return is_palindrome_recursive(word, low + 1, high - 1)
-    
