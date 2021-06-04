ae2accedfa0cf60ffae6be47a58f642fb812c3b3
vbergadev <vbergadev@gmail.com>
2021-06-04 15:47:34 -0300

    O PyTaOn

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    """
+    Find zip function (https://realpython.com/python-zip-function/),
+    zip works as a parallel iterator (Tuple iterator)
+    """
+    counter = 0
+    for tempo1, tempo2 in zip(start_time, end_time):
+        if target_time >= tempo1 and target_time <= tempo2:
+            counter += 1
+    return counter
