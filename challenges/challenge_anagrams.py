95ba1fa61fc2b588fef60fd384e3bdb1dda121c3
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-06-04 09:18:55 -0300

    :cake: correções do flake8 e do black

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,6 +1,5 @@
-
 def is_anagram(first_string, second_string):
-# Retorne false se a primeira palavra ou a segunda palavra for uma string vazia
+    # Retorne false se a primeira palavra ou a segunda palavra for uma string vazia
     if not first_string or not second_string:
         return False
 
@@ -10,4 +9,3 @@ def is_anagram(first_string, second_string):
         if letra not in second_string:
             return False
     return True
-    
