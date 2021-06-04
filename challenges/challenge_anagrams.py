689a5d61253155d03253625676c76340c6921cb3
ricknois <rickssti@outlook.com>
2021-06-04 19:50:14 -0300

    :sparkles: ..

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,6 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string == "" or second_string == "":
+        return False
+    if sorted(first_string) != sorted(second_string):
+        return False
+    return True
