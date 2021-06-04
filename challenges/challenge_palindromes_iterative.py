e0e778585493b2e4fbfaef7aacdcc0f5f3fa9511
Luiz E F Simoes <luizefsimoes@gmail.com>
2021-06-03 22:19:25 -0300

    Subindo requisito 5 - Um dia faço 100%

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,7 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    # Fazendo sem FOR
+    # Macete do fatiamento que aprendi com o Guanabara
+    # https://www.youtube.com/watch?v=5VBWe6BXzRo
+    if word and word == word[::-1]:
+        return True
+    return False
