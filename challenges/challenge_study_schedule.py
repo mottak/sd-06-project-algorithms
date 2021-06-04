8e026b1014923e06f79c1a54de4359cacec2ffc0
elias.carvalho <elias.carvalho@autonomoz.com.br>
2021-06-03 23:09:55 -0300

    fix

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,7 +2,9 @@ def study_schedule(start_time, end_time, target_time):
     if len(start_time) + len(end_time) + target_time == 0:
         return 0
 
-    return sum(
-        start_time[n] <= target_time <= end_time[n]
-        for n in range(len(start_time))
-    )
+    counter = 0
+    for n in range(len(start_time)):
+        if start_time[n] <= target_time <= end_time[n]:
+            counter += 1
+
+    return counter
