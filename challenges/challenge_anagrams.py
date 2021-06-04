408a6e8285459cc1d74346eef988f44395563bd2
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-06-04 16:41:19 -0300

    :sparkles: feat(challenges): Requirement 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,9 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string == '' or second_string == '' or len(first_string) != len(second_string):
+        return False
+
+    for x in range(0, len(first_string)):
+        if first_string[x] not in second_string:
+            return False
+    
+    return True
